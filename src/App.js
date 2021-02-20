e2a780ffbe2e7afd939ceaca4d32f7cdb3f69211
xMiCx <70638947+Mickuppom@users.noreply.github.com>
2021-02-20 13:22:52 +0700

    ...

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
+import HomeScreen from "./screen/HomeScreen";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount,] = useState(0);
 
-    const addCount = () => {
-      setCount(count+1);
+  const addCount = (type = "add") => {
+    if (type === "add") {
+      setCount(count + 1);
     }
+    else {
+      setCount(count - 1);
+    }
+  }
   return (
     <div>
+      <HomeScreen>3.</HomeScreen>
       <p>You did it {count} time(s)!</p>
-      <button onClick={addCount}>+1</button>
+      <button onClick={() => addCount("add")}>+1</button>
+      <button onClick={() => addCount("minus")}>-1</button>
     </div>
   );
 }
