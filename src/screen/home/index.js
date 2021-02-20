251426334cc12272c31169dc9a8faf627caa3383
xMiCx <70638947+Mickuppom@users.noreply.github.com>
2021-02-20 15:36:59 +0700

    ...........

--- src/screen/home/index.js
+++ src/screen/home/index.js
@@ -1,8 +1,51 @@
+import { useState } from "react";
+import React from 'react';
+import {
+  Collapse,
+  Container,
+  Jumbotron,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
   return (
     <div>
-      <h1>Home Page</h1>
-      <button className="btn btn-danger">FUCK</button>
+      <Container>
+        <Jumbotron>
+          <h1>Home Page</h1>
+        </Jumbotron>
+        <div>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">My Home</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap">
+                    About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="/components/">Contact</NavLink>
+                </NavItem>
+              </Nav>
+              <NavbarText>Simple Text</NavbarText>
+            </Collapse>
+          </Navbar>
+        </div>
+      </Container>
     </div>
   );
 };
