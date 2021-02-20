593f2e2555494ed6de4222a0bd5b023be5e6b020
xMiCx <70638947+Mickuppom@users.noreply.github.com>
2021-02-20 15:54:45 +0700

    ....

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './screen/home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './screen/auth/login';
 
 function App() {
   return <div>
     <Router>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
       </Switch>
     </Router>
   </div>
